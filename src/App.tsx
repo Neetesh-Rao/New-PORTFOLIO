6931e7bf4b847258af4664a4f39e6768e69a963a
Neetesh Kumar Yadav <neeteshyadav0206@gmail.com>
2026-09-02 12:40:35 +0530

    d

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <main className="page-shell">
+      <img
+        className="profile-photo"
+        src="/images/notebook-profile.png"
+        alt="A handwritten profile on a lined notebook beside a keyboard"
+      />
+    </main>
+  );
+}
